0d6b8b36fae409dcbaac0ea8eeb66fca44788d20
Harsh Bharwani <harshbharwani1234@gmail.com>
2025-01-06 20:58:19 +0530

    adding the draw feature

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -3,6 +3,7 @@ let boxes=document.querySelectorAll("button");
 let resetButton=document.querySelector("#reset-button");
 // let newGame=document.querySelector("#new-game-button");
 let winMsg=document.querySelector(".msg-container");
+let total_count=0;
 let winPatterns=[
     [0,1,2],
     [3,4,5],
@@ -17,6 +18,7 @@ let winPatterns=[
 boxes.forEach((box)=>{
     if(box.getAttribute("id")!=="reset-button"){
         box.addEventListener("click", ()=>{
+            winMsg.classList.add("hide");
             if(turn==="O") {
                 box.innerText="O";
                 turn="X";
@@ -27,28 +29,58 @@ boxes.forEach((box)=>{
             }
             box.disabled=true;
             checkWinner();
+            total_count++;    
+            if(total_count==9){
+                drawMsg();
+            }
         });
     }
 });
-resetButton.addEventListener("click", ()=>{
+function drawMsg(){
+    winMsg.innerText=`It's a draw..`;
+    winMsg.classList.remove("hide");
+    total_count=0;
+}
+// function checktie(){
+//     tie=true;
+//     for(box of boxes){
+//         if(box.disabled==false){
+//             tie=false;
+//             break;
+//         }
+//     }
+//     console.log(tie);
+//     if(tie) {
+//         printWinner("It's a tie..");
+//     }
+// }
+function enable_button(){
     for(box of boxes){
-        if(box.getAttribute("id")!="reset-button")
-        box.innerText="";
-        box.disabled=false;
+        if(box.getAttribute("id")!="reset-button"){
+            box.innerText="";
+            box.disabled=false;
+        }
     }
+}
+resetButton.addEventListener("click", ()=>{
+    enable_button();
     turn="O";
+    winMsg.classList.add("hide");
+    total_count=0;
 });
 
 function printWinner(arg){
     winMsg.innerText=`Congratulations, winner is ${arg}`;
     winMsg.classList.remove("hide");
+    total_count=0;
 }
 function checkWinner(){
     for(pattern of winPatterns){
         if(boxes[pattern[0]].innerText!="" && boxes[pattern[0]].innerText== boxes[pattern[1]].innerText && boxes[pattern[1]].innerText== boxes[pattern[2]].innerText ) {
             printWinner(boxes[pattern[0]].innerText);
-            resetButton.click();
+            enable_button();
             break;
         }
     }
 }
+console.log(total_count);
