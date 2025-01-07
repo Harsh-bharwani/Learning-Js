3a958edeb44a0b7521b188a69f609a8cbfe3bfd4
Harsh Bharwani <harshbharwani1234@gmail.com>
2025-01-07 18:36:00 +0530

    almost complete

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,4 +1,5 @@
-let turn="O"; // X 
+let turn=document.querySelector("#turn"); // X 
+let turn_container=document.querySelector(".turn-container");
 let boxes=document.querySelectorAll("button");
 let resetButton=document.querySelector("#reset-button");
 // let newGame=document.querySelector("#new-game-button");
@@ -14,47 +15,44 @@ let winPatterns=[
     [0,4,8],
     [2,4,6]
 ];
-
-boxes.forEach((box)=>{
-    if(box.getAttribute("id")!=="reset-button"){
-        box.addEventListener("click", ()=>{
-            winMsg.classList.add("hide");
-            if(turn==="O") {
+let game=document.querySelector(".game");
+game.addEventListener("click", (e)=>{
+    let box=e.target;   
+    if(total_count==0) {
+        enable_button();
+    }
+    if(box.getAttribute("class")!="game"){      
+            if(turn.innerText==="O") {
                 box.innerText="O";
-                turn="X";
+                turn.innerText="X";                
             }
             else{
                 box.innerText="X"; 
-                turn="O";
+                turn.innerText="O";
             }
             box.disabled=true;
             checkWinner();
             total_count++;    
             if(total_count==9){
-                drawMsg();
-            }
-        });
-    }
+                drawMsg();   
+            };
+    };
 });
+    
 function drawMsg(){
-    winMsg.innerText=`It's a draw..`;
-    winMsg.classList.remove("hide");
+    winMsg.innerText=`It's a draw.. 🤝`;
     total_count=0;
+    disable_button();
+    printResetMsg();
 }
-// function checktie(){
-//     tie=true;
-//     for(box of boxes){
-//         if(box.disabled==false){
-//             tie=false;
-//             break;
-//         }
-//     }
-//     console.log(tie);
-//     if(tie) {
-//         printWinner("It's a tie..");
-//     }
-// }
+function printResetMsg(){
+    turn_container.classList.add("print-reset-msg");
+    turn_container.innerHTML="<marquee scrollamount='10' direction='left'>Please Reset the game!</marquee>";
+    turn_container.classList.add("print-reset-msg");
+}
 function enable_button(){
+    turn.innerText="O";
+    winMsg.classList.add("hide");
     for(box of boxes){
         if(box.getAttribute("id")!="reset-button"){
             box.innerText="";
@@ -62,23 +60,34 @@ function enable_button(){
         }
     }
 }
+function disable_button(){
+    turn.innerText="O";
+    winMsg.classList.remove("hide");
+    for(box of boxes){
+        if(box.getAttribute("id")!="reset-button"){
+            box.disabled=true;
+        }
+    }
+}
 resetButton.addEventListener("click", ()=>{
     enable_button();
-    turn="O";
     winMsg.classList.add("hide");
     total_count=0;
+    turn_container.classList.remove("print-reset-msg");
+    turn_container.innerHTML="<span>Turn: </span><div id='turn'>O</div>"
 });
 
 function printWinner(arg){
-    winMsg.innerText=`Congratulations, winner is ${arg}`;
+    winMsg.innerText=`Congratulations, winner is ${arg} 🏆`;
     winMsg.classList.remove("hide");
     total_count=0;
+    disable_button();
+    printResetMsg()
 }
 function checkWinner(){
     for(pattern of winPatterns){
         if(boxes[pattern[0]].innerText!="" && boxes[pattern[0]].innerText== boxes[pattern[1]].innerText && boxes[pattern[1]].innerText== boxes[pattern[2]].innerText ) {
             printWinner(boxes[pattern[0]].innerText);
-            enable_button();
             break;
         }
     }
